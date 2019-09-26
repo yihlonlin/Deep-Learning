c59457a6b4258f488a49fd7574ad6e28c3a88c63
SBKPB <ben12505ca@gmail.com>
2019-09-27 01:44:20 +0800

    class3更新

---
+++ Lesson_02/Code/04_gensim/04_Lesson.py
@@ -0,0 +1,75 @@
+
+# coding: utf-8
+
+
+from gensim.models import Word2Vec
+# define training data
+sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
+            ['this', 'is', 'the', 'second', 'sentence'],
+            ['yet', 'another', 'sentence'],
+            ['one', 'more', 'sentence'],
+            ['and', 'the', 'final', 'sentence']]
+# train model
+model = Word2Vec(sentences, min_count=1, window=5, sg=0, size=10)
+# summarize the loaded model
+print(model)
+# summarize vocabulary
+words = list(model.wv.vocab)
+print(words)
+# access vector for one word
+print(model['sentence','is'])
+print(model.similarity('sentence','is'))
+print(model.most_similar('sentence'))
+
+
+from gensim.models import Word2Vec
+from sklearn.decomposition import PCA
+from matplotlib import pyplot
+# define training data
+sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
+            ['this', 'is', 'the', 'second', 'sentence'],
+            ['yet', 'another', 'sentence'],
+            ['one', 'more', 'sentence'],
+            ['and', 'the', 'final', 'sentence']]
+# train model
+model = Word2Vec(sentences, window=5, min_count=1)
+# fit a 2d PCA model to the vectors
+X = model[model.wv.vocab]
+pca = PCA(n_components=2)
+result = pca.fit_transform(X)
+# create a scatter plot of the projection
+pyplot.scatter(result[:, 0], result[:, 1])
+words = list(model.wv.vocab)
+for i, word in enumerate(words):
+    pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+pyplot.show()
+
+
+from gensim.models import KeyedVectors
+# load the google word2vec model
+filename = 'GoogleNews-vectors-negative300.bin'
+model = KeyedVectors.load_word2vec_format(filename, binary=True)
+# calculate: (king - man) + woman = ?
+result = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
+print(result)
+#[('queen', 0.7698540687561035)]
+
+
+from gensim.models import KeyedVectors
+from gensim.scripts.glove2word2vec import glove2word2vec
+
+# convert glove to word2vec format
+glove_input_file = 'glove.6B.100d.txt'
+word2vec_output_file = 'glove.6B.100d.txt.word2vec'
+glove2word2vec(glove_input_file, word2vec_output_file)
+
+# load the converted model
+filename = 'glove.6B.100d.txt.word2vec'
+model = KeyedVectors.load_word2vec_format(filename, binary=False)
+# calculate: (king - man) + woman = ?
+result = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
+print(result)
+
+
+
+
---
+++ Lesson_02/Code/04_gensim/readme.txt
@@ -0,0 +1,2 @@
+pip install gensim
+http://cpmarkchang.logdown.com/posts/772665-nlp-vector-space-semantics
